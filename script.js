a46e72061ab7b86ad22270eb4a5ddc06495db479
Tanveer khan <152361941+tanveerkhan00@users.noreply.github.com>
2024-06-27 10:55:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var marks;
+marks = +prompt("Enter your marks")
+if(marks >=90 &&  <=100)
+    {
+    alert("your marks is" + marks + "and your grade is A+")
+    }
+    else if(marks >=80 &&  <=89)
+        {
+        alert("your marks is" + marks + "and your grade is A")
+        }
+        else if(marks >=70 &&  <=79)
+            {
+            alert("your marks is" + marks + "and your grade is B")
+            } else if(marks >=60 &&  <=69)
+                {
+                alert("your marks is" + marks + "and your grade is C")
+                } else if(marks >=50 &&  <=59)
+                    {
+                    alert("your marks is" + marks + "and your grade is D")
+                    } else if(marks >=80 &&  <=89)
+                        {
+                        alert("congrats you have fial")
+                        }
